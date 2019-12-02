0d771fb8db1d6a03fa03597c05644132518ea04b
Sébastien Doubey <sebastien.doubey@gmail.com>
2019-12-02 19:08:20 +0100

    Add Inter font

--- src/main/java/com/spreeng/message/organization/OrganizationFont.java
+++ src/main/java/com/spreeng/message/organization/OrganizationFont.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum OrganizationFont {
+  INTER("'Inter', sans-serif"),
   ROBOTO("'Roboto', sans-serif"),
   OPEN_SANS("'Open Sans', sans-serif"),
   MONTSERRAT("'Montserrat', sans-serif");
