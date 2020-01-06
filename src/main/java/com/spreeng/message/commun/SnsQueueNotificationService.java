70666099af2e82275c0874f3e6be1dfceefbe034
akhoufi <a.khoufi@gmail.com>
2020-01-06 01:55:35 +0100

    Add SnsQueueNotificationService class

---
+++ src/main/java/com/spreeng/message/commun/SnsQueueNotificationService.java
@@ -0,0 +1,25 @@
+package com.spreeng.message.commun;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.spreeng.message.commun.QueueNotificationService;
+
+import eu.lycoris.spring.common.LycorisSubjectMessage;
+import eu.lycoris.spring.sns.LycorisSnsManager;
+
+@Component
+public class SnsQueueNotificationService implements QueueNotificationService {
+
+  @Autowired private LycorisSnsManager snsManager;
+
+  @Override
+  public boolean sendMessage(String topic, LycorisSubjectMessage message) {
+    try {
+      snsManager.sendMessage(topic, message);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
