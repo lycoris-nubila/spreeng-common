452cc15297d76ec412ff0190ddb3d2b1e2f2b17f
sdoubey <sebastien.doubey@gmail.com>
2020-02-04 16:46:54 +0100

    Update 1.0.31

--- src/main/java/com/spreeng/common/common/LocalizedId.java
+++ src/main/java/com/spreeng/common/common/LocalizedId.java
@@ -16,41 +16,15 @@ import lombok.Setter;
 @Setter
 @Embeddable
 @BatchSize(size = 100)
+@SuppressWarnings("serial")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LocalizedId implements Serializable {
-
-  private static final long serialVersionUID = 1089196571270403924L;
 
   private UUID id;
 
   private String locale;
 
   public LocalizedId(String locale) {
-    super();
     this.locale = locale;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
-    LocalizedId other = (LocalizedId) obj;
-    if (locale == null) {
-      if (other.locale != null) return false;
-    } else if (!locale.equals(other.locale)) return false;
-    if (id == null) {
-      if (other.id != null) return false;
-    } else if (!id.equals(other.id)) return false;
-    return true;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((locale == null) ? 0 : locale.hashCode());
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
   }
 }
