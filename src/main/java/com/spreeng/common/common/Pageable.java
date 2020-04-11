a17c7daeda24a432f8f471850f460b4bd9ce663a
Sébastien Doubey <sebastien.doubey@gmail.com>
2020-04-12 00:14:00 +0200

    Update pageable

--- src/main/java/com/spreeng/common/common/Pageable.java
+++ src/main/java/com/spreeng/common/common/Pageable.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Pageable<T> {
-  private Long totalElements;
-  private List<T> elements;
+
   private String nextPageToken;
+
+  @NonNull private List<T> elements;
+
+  @NonNull private Long totalElements;
 }
